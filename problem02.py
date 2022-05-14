ea54fb64263a34ed014096e4ecdef97df4f29f1c
apt-bit <60715671+apt-bit@users.noreply.github.com>
2022-05-14 14:15:32 +0100

    more elegant

--- problem02.py
+++ problem02.py
@@ -1,13 +1,11 @@
-#Far too slow just using recursion
+def addfib(L):
+    L.append(L[-1]+L[-2])
 
 def main():
-    fibs = [1,2]
-    while True:
-        nextfib = fibs[-1] + fibs[-2]
-        if nextfib > 4000000:
-            break
-        fibs.append(nextfib)
-    print('Sum of even fibonacci terms under four million: {}'.format(sum([fib for fib in fibs if fib%2==0])))
+    fib = [1,2]
+    while fib[-1] < 4000000:
+        addfib(fib)
+    print(sum([x for x in fib if x%2==0]))
 
 if __name__ == '__main__':
     main()
