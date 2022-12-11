e16e7717b7e927558e408aeae7ea07d23576bdf0
apt-bit <60715671+apt-bit@users.noreply.github.com>
2022-12-11 18:02:26 +0000

    works now
    bit janky but I like that

--- problem19incomplete.py
+++ problem19incomplete.py
@@ -1,21 +1,31 @@
 """
-start on 1 jan 1900 (monday),
-go through to the first of the next month
-1 feb 1900,
-figure out which day it is (req number days in jan and which day was the first of jan)
-if sunday add 1 to counter
-continue until 1 dec 2000
-sort special cases after
-
+0:mon
+1:tues etc
+1 jan 1900 was a monday
+months: 31, 28/29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+first compute without leap years taken into account
 """
 
+def sunday():
+    sundaycount = 0
+    day = 1 #first day of 1901 is a tuesday
+    months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    for year in range(1901, 2001):
+        if (year %4 == 0):
+            months[1] = 29
+        for i in range(len(months)):
+            day += months[i]
+            day %= 7
+            if day == 6:
+                sundaycount+=1
+        months[1] = 28
 
-def what_day_am_i(NumberOfDaysInPreviousMonth, DayFirstOfPreviousMonth):
-    days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-    while NumberOfDaysInPreviousMonth >= 7:
-        NumberOfDaysInPreviousMonth %= 7
-    NumberOfLastMonth = days.index(DayFirstOfPreviousMonth)
-    CurrentDay = days[NumberOfLastMonth + NumberOfDaysInPreviousMonth]
-    return CurrentDay
+    print(day)
+    print(sundaycount)
 
-print(what_day_am_i(31,"Monday"))
+
+def main():
+    sunday()
+
+if __name__ == '__main__':
+    main()
